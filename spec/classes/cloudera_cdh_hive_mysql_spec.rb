91e278c618790ac8892553e30287ee0a9099e685
Michael Arnold <github@razorsedge.org>
2013-01-30 00:16:39 -0700

    Added the rest of the rspec tests.

---
+++ spec/classes/cloudera_cdh_hive_mysql_spec.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'cloudera::cdh::hive::mysql', :type => 'class' do
+
+#  context 'on a non-supported operatingsystem' do
+#    let :facts do {
+#      :osfamily        => 'foo',
+#      :operatingsystem => 'bar'
+#    }
+#    end
+#    it 'should fail' do
+#      expect {
+#        should raise_error(Puppet::Error, /Module cloudera is not supported on bar/)
+#      }
+#    end
+#  end
+
+  context 'on a supported operatingsystem, default parameters' do
+    let(:params) {{ :password => 'myPass' }}
+    let :facts do {
+      :osfamily        => 'RedHat',
+      :operatingsystem => 'CentOS'
+    }
+    end
+    it { should contain_class('mysql::java') }
+    it { should contain_file('/usr/lib/hive/lib/mysql-connector-java.jar').with(
+      :ensure => 'link',
+      :target => '/usr/share/java/mysql-connector-java.jar'
+    )}
+#    it { should contain_mysql::db('metastoredb').with(
+#      :user     => 'hive',
+#      :password => 'myPass',
+#      :host     => '%',
+#      :grant    => [ 'select_priv', 'insert_priv', 'update_priv', 'delete_priv', ],
+#      :sql      => '/usr/lib/hive/scripts/metastore/upgrade/mysql/hive-schema-0.9.0.mysql.sql'
+#    )}
+  end
+end
