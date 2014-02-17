1baa63422c075f5e9568987ad18f84ee35ed5f37
Michael Arnold <github@razorsedge.org>
2014-02-17 14:56:59 -0700

    Revamp Java alternatives.
    Anchor all resources in Class['cloudera::java'].
    Install File['java-profile.d'] on Debian and Ubuntu as well as RedHat
    and SLES.
    Update the Java alternatives on RedHat and Suse to be more like the
    Debian package with binary and manpage links in FHS locations.  Also
    support removing the links.

--- files/java.sh
+++ files/java.sh
@@ -1,5 +1,9 @@
 ###
 ### File managed by Puppet
 ###
-export JAVA_HOME=/usr/java/default
-export PATH=$PATH:$JAVA_HOME/bin
+if [ -d /usr/java/default ]; then
+  export JAVA_HOME=/usr/java/default
+elif [ -d /usr/lib/jvm/j2sdk1.6-oracle ]; then
+  export JAVA_HOME=/usr/lib/jvm/j2sdk1.6-oracle
+fi
+#export PATH=$PATH:$JAVA_HOME/bin
