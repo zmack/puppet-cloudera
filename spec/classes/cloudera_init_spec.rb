ebe72fc74c1ffb1c661bb95452580af7f01a3035
Michael Arnold <github@razorsedge.org>
2014-03-06 00:36:42 -0700

    Update rspec tests for main class.

--- spec/classes/cloudera_init_spec.rb
+++ spec/classes/cloudera_init_spec.rb
@@ -25,15 +25,22 @@ describe 'cloudera', :type => 'class' do
     }
     end
     it { should compile.with_all_deps }
-    it { should contain_class('cloudera::java').with_ensure('present') }
+    it { should contain_class('cloudera::java5').with_ensure('present') }
+    it { should_not contain_class('cloudera::java5::jce') }
+    it { should contain_class('cloudera::cm5::repo').with_ensure('present') }
+    it { should contain_class('cloudera::cm5').with_ensure('present') }
+    it { should_not contain_class('cloudera::cdh5::repo') }
+    it { should_not contain_class('cloudera::cdh5') }
+
+    it { should_not contain_class('cloudera::java') }
     it { should_not contain_class('cloudera::java::jce') }
-    it { should contain_class('cloudera::cm::repo').with_ensure('present') }
-    it { should contain_class('cloudera::cm').with_ensure('present') }
+    it { should_not contain_class('cloudera::cm::repo') }
+    it { should_not contain_class('cloudera::cm') }
     it { should_not contain_class('cloudera::cdh::repo') }
     it { should_not contain_class('cloudera::cdh') }
   end
 
-  context 'on a supported operatingsystem, custom parameters' do
+  context 'on a supported operatingsystem, custom parameters, cm_version => 5' do
     let :facts do {
       :osfamily        => 'RedHat',
       :operatingsystem => 'CentOS'
@@ -42,17 +49,77 @@ describe 'cloudera', :type => 'class' do
 
     describe 'ensure => absent' do
       let(:params) {{ :ensure => 'absent' }}
+      it { should contain_class('cloudera::java5').with_ensure('absent') }
+      it { should_not contain_class('cloudera::java5::jce') }
+      it { should contain_class('cloudera::cm5').with_ensure('absent') }
+      it { should contain_class('cloudera::cm5::repo').with_ensure('absent') }
+    end
+
+    describe 'use_parcels => false' do
+      let(:params) {{ :use_parcels => false }}
+      it { should contain_class('cloudera::java5').with_ensure('present') }
+      it { should_not contain_class('cloudera::java5::jce') }
+      it { should contain_class('cloudera::cm5').with_ensure('present') }
+      it { should contain_class('cloudera::cm5::repo').with_ensure('present') }
+      it { should contain_class('cloudera::cdh5::repo').with_ensure('present') }
+      it { should contain_class('cloudera::cdh5').with_ensure('present') }
+      it { should_not contain_class('cloudera::cdh::repo') }
+      it { should_not contain_class('cloudera::cdh') }
+      it { should_not contain_class('cloudera::impala::repo') }
+      it { should_not contain_class('cloudera::impala') }
+      it { should_not contain_class('cloudera::search::repo') }
+      it { should_not contain_class('cloudera::search') }
+    end
+
+    describe 'use_parcels => false, cdh_version => 4' do
+      let(:params) {{ :use_parcels => false, :cdh_version => '4' }}
+      it { should contain_class('cloudera::java5').with_ensure('present') }
+      it { should_not contain_class('cloudera::java5::jce') }
+      it { should contain_class('cloudera::cm5').with_ensure('present') }
+      it { should contain_class('cloudera::cm5::repo').with_ensure('present') }
+      it { should contain_class('cloudera::cdh::repo').with_ensure('present') }
+      it { should contain_class('cloudera::cdh').with_ensure('present') }
+      it { should contain_class('cloudera::impala::repo').with_ensure('present') }
+      it { should contain_class('cloudera::impala').with_ensure('present') }
+      it { should contain_class('cloudera::search::repo').with_ensure('present') }
+      it { should contain_class('cloudera::search').with_ensure('present') }
+      it { should_not contain_class('cloudera::cdh5::repo') }
+      it { should_not contain_class('cloudera::cdh5') }
+    end
+
+    describe 'install_java => false' do
+      let(:params) {{ :install_java => false }}
+      it { should_not contain_class('cloudera::java5') }
+      it { should_not contain_class('cloudera::java5::jce') }
+    end
+
+    describe 'install_jce => true' do
+      let(:params) {{ :install_jce => true }}
+      it { should contain_class('cloudera::java5').with_ensure('present') }
+      it { should contain_class('cloudera::java5::jce').with_ensure('present') }
+    end
+  end
+
+  context 'on a supported operatingsystem, custom parameters, cm_version => 4' do
+    let :facts do {
+      :osfamily        => 'RedHat',
+      :operatingsystem => 'CentOS'
+    }
+    end
+
+    describe 'ensure => absent' do
+      let(:params) {{ :ensure => 'absent', :cm_version => '4' }}
       it { should contain_class('cloudera::java').with_ensure('absent') }
       it { should contain_class('cloudera::cm').with_ensure('absent') }
       it { should contain_class('cloudera::cm::repo').with_ensure('absent') }
     end
 
     describe 'use_parcels => false' do
-      let(:params) {{ :use_parcels => false }}
+      let(:params) {{ :use_parcels => false, :cm_version => '4' }}
       it { should contain_class('cloudera::java').with_ensure('present') }
       it { should contain_class('cloudera::cm').with_ensure('present') }
-      it { should contain_class('cloudera::cdh::repo').with_ensure('present') }
       it { should contain_class('cloudera::cm::repo').with_ensure('present') }
+      it { should contain_class('cloudera::cdh::repo').with_ensure('present') }
       it { should contain_class('cloudera::cdh').with_ensure('present') }
       it { should contain_class('cloudera::impala::repo').with_ensure('present') }
       it { should contain_class('cloudera::impala').with_ensure('present') }
@@ -61,15 +128,29 @@ describe 'cloudera', :type => 'class' do
     end
 
     describe 'install_java => false' do
-      let(:params) {{ :install_java => false }}
+      let(:params) {{ :install_java => false, :cm_version => '4' }}
       it { should_not contain_class('cloudera::java') }
       it { should_not contain_class('cloudera::java::jce') }
     end
 
     describe 'install_jce => true' do
-      let(:params) {{ :install_jce => true }}
+      let(:params) {{ :install_jce => true, :cm_version => '4' }}
       it { should contain_class('cloudera::java').with_ensure('present') }
       it { should contain_class('cloudera::java::jce').with_ensure('present') }
+    end
+  end
+
+  context 'on a supported operatingsystem, custom parameters, cm_version => blah' do
+    let(:params) {{ :cm_version => 'blah' }}
+    let :facts do {
+      :osfamily        => 'RedHat',
+      :operatingsystem => 'CentOS'
+    }
+    end
+    it do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Parameter \$cm_version must start with either 4 or 5./)
     end
   end
 
